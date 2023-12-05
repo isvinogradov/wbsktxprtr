be86054149bfd62e9e956099001cf15777c2951a
Ivan Vinogradov <isvinogradov@gmail.com>
2023-12-05 20:14:50 +0200

    add logging; add test websocket server

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # wbsktxprtr
 
 ```python main.py```
+
+Optionally you can start test websocket on localhost:  
+```python ws_server_test.py```
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 from time import perf_counter
 from typing import Iterable
 
@@ -8,12 +9,14 @@ from prometheus_client import (
     REGISTRY,
 )
 from prometheus_client.metrics_core import (
-    GaugeMetricFamily,
     Metric,
+    GaugeMetricFamily,
 )
 from prometheus_client.registry import Collector
 
-URI_LIST = ['ws://ssssss', 'wss://fffffffffff']
+logging.basicConfig(level=logging.INFO)
+
+URI_LIST = ['ws://127.0.0.1:8001']
 SLEEP_DELAY = 10
 PROMETHEUS_PORT = 9000
 PROMETHEUS_ADDR = '127.0.0.1'
@@ -25,32 +28,40 @@ class ProbeResult:
         self.is_up = is_up
         self.latency: float = round(latency, 2)
 
+    def __str__(self):
+        return f"{self.uri}(UP = {self.is_up}) - {self.latency}ms"
+
 
 async def get_probe_results() -> list[ProbeResult]:
     results = []
     for uri in URI_LIST:
         t_start = perf_counter()
-        async with websockets.connect(uri) as ws:
+        try:
+            async with websockets.connect(uri) as ws:
+                latency_ms = (perf_counter() - t_start) * 1000
+                results.append(ProbeResult(uri, bool(ws), latency_ms))
+        except ConnectionRefusedError:
             latency_ms = (perf_counter() - t_start) * 1000
-            results.append(ProbeResult(uri, bool(ws), latency_ms))
+            results.append(ProbeResult(uri, False, latency_ms))
     return results
 
 
 class ResultCollector(Collector):
     def collect(self) -> Iterable[Metric]:
         for result in asyncio.run(get_probe_results()):
+            logging.info(result)
             yield GaugeMetricFamily(
                 'websocket_probe_success',
                 '1 if websocket is up, 0 otherwise',
                 value=int(result.is_up),
-                labels=[result.uri]
+                # labels=[result.uri]
             )
             yield GaugeMetricFamily(
                 'websocket_probe_latency',
-                'latency in connection',
+                'connection latency',
                 value=result.latency,
                 unit='milliseconds',
-                labels=[result.uri]
+                # labels=[result.uri]
             )
 
 
